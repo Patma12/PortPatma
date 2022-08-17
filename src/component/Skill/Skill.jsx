ff89f4fb9630e7b59bf76ffd45788cfdeaf5fb21
Patma12 <107095346+Patma12@users.noreply.github.com>
2022-08-17 21:54:50 +0700

    a

--- src/component/Skill/Skill.jsx
+++ src/component/Skill/Skill.jsx
@@ -1,19 +1,20 @@
 import './Skill.css'
+import html from '../../assets/image/html.png'
 
 function Skill() {
     return (
         <div className='skill-box'>
             <h2 className='container-box '>WHAT ABILITIES I CAN OFFER</h2>
             <div className='skill container-box '>
-                <img src="../src/assets/image/html.png" alt='html' />
-                <img src="../src/assets/image/css.png" alt='css' />
+                <img src={html} alt='html' />
+                {/* <img src="../src/assets/image/css.png" alt='css' />
                 <img src="../src/assets/image/javascript.png" alt='javascript' />
                 <img src="../src/assets/image/react.png" alt='react' />
                 <img src="../src/assets/image/nodejs.png" alt='nodejs' />
                 <img src="../src/assets/image/expressjs.png" alt='expressjs' />
                 <img src="../src/assets/image/python.png" alt='python' />
                 <img src="../src/assets/image/mongodb.png" alt='mongodb' />
-                
+                 */}
             </div>
         </div>
     )
