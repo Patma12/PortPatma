aff2f427a1af581d80cb951dcc6ced5dc97f679e
Patma12 <107095346+Patma12@users.noreply.github.com>
2022-08-18 23:00:56 +0700

    update

--- src/component/Nav/Nav.jsx
+++ src/component/Nav/Nav.jsx
@@ -8,9 +8,16 @@ function Nav() {
     <nav>
         <ul className="topnav">
             <li><a href="#home">Home</a></li>
+            <li><a href="#Ability">Ability</a></li>
             <li><a href="#Project">Project</a></li>
             <li><a href="#Education">Education</a></li>
             <li><a href="#Experince">Experince</a></li>
+            <a href=''><button className='fa-button'>Resume</button></a>
+            <a href="#" className="fa fa-facebook"></a>
+            <a href="https://www.linkedin.com/in/patma-mataa-287672248/" className="fa fa-linkedin"></a>
+            <a href="https://github.com/Patma12" className="fa fa-github"></a>
+            
+           
             
         </ul>
     </nav>
--- src/component/Profile/Profile.jsx
+++ src/component/Profile/Profile.jsx
@@ -9,13 +9,13 @@ function Profile() {
             <div className='profile-grid '>
                 <div className='profile-in'>
                     <h1>Hello! I'm <span className='orange'>Patma Mataa</span><br />
-                    I'm a full-stack web developer.👑</h1>
-                    <p>I'm a tech geek specializing in building (and occasionally designing) exceptional digital experiences. </p>
-                    <p>“Life without knowledge is death in disguise.”  - Talib Kweli</p>
-                    <a href=''><button className='fa-button'>get resume</button></a>
+                    I'm a full-stack web developer</h1>
+                    <p className='p'>&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;Because coding is like magic, it makes we can communicate with each other around the world. Nowadays, technology plays an important role in our lives. So I started to learn these skills every single day. I felt love and excited about it all the time. Now looking for work with an entry-level software developer with your organization to walk forward together. I am confident that my experience working in the position of services over the past four years has been combined with my technical skill. I have learned and gotten from Software Developer Bootcamp by Generation Thailand taught me technical skills like the MERN stack, virtual works roleplay and soft skills like design thinking, agile, scrum process, etc. These will be tools to help us move forward and make new things happen. </p>
+                    {/* <p>“Life without knowledge is death in disguise.”  - Talib Kweli</p> */}
+                    {/* <a href=''><button className='fa-button'>get resume</button></a>
                     <a href="#" className="fa fa-facebook"></a>
                     <a href="https://www.linkedin.com/in/patma-mataa-287672248/" className="fa fa-linkedin"></a>
-                    <a href="https://github.com/Patma12" className="fa fa-github"></a>
+                    <a href="https://github.com/Patma12" className="fa fa-github"></a> */}
                 </div>
                 <div className=''>
                     <img className='profile-image' src={patma}  alt="profile-image" />
